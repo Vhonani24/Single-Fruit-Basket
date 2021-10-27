c11e2e4ae2d62618748ceba81538b3a880cdd660
Vhonani24 <vhonani.ramu@gmail.com>
2021-10-27 17:07:59 +0200

    committed changes

--- test/the-balloon-shop.test.js
+++ test/the-balloon-shop.test.js
@@ -3,7 +3,7 @@ let TheBalloonShop = require("../the-balloon-shop");
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/my_balloon_tests';
+const connectionString = process.env.DATABASE_URL || 'postgresql://vhonani:vhonani123@localhost:5432/my_balloon_tests';
 
 const pool = new Pool({
     connectionString
