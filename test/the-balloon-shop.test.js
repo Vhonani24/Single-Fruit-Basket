c2bc84440582b4b58918a54afa46e7c59759337f
Makhosandile <makhosandile@projectcodex.co>
2021-10-27 12:48:58 +0200

    formated the test file

--- test/the-balloon-shop.test.js
+++ test/the-balloon-shop.test.js
@@ -10,44 +10,37 @@ const pool = new Pool({
 });
 
 
-describe('The balloon function', function(){
+describe('The balloon function', function () {
 
 
-    beforeEach(async function(){
+    beforeEach(async function () {
         // clean the tables before each test run
         await pool.query("delete from;");
     });
 
-    it('should get the valid color', function(){
-        assert.equal(true,false );
+    it('should get the valid color', function () {
+        assert.equal(true, false);
     });
 
-    it('should get in-valid color', function(){
-        assert.equal(true,false );
+    it('should get in-valid color', function () {
+        assert.equal(true, false);
     });
-  
 
-    it('should return count for a specific color', function(){
-        assert.equal(true,false );
+    it('should return count for a specific color', function () {
+        assert.equal(true, false);
     })
 
-    it('should get all the colors', function(){
-        assert.equal(true,false );
-
-    })
-
-
-
-    it('should get all the colors that are requested', function(){
-        assert.equal(true,false );
+    it('should get all the colors', function () {
+        assert.equal(true, false);
 
     })
 
+    it('should get all the colors that are requested', function () {
+        assert.equal(true, false);
 
+    })
 
-    
-
-    after(function(){
+    after(function () {
         pool.end();
     })
 });
