05c4f59ff2998c626beab7e3a2bcbc02ca9da21f
Unalo <unalomtshokotsha@gmail.com>
2021-10-27 12:17:37 +0200

    added files and test

---
+++ test/the-balloon-shop.test.js
@@ -0,0 +1,28 @@
+let assert = require("assert");
+let greet = require("../the-balloon-shop");
+const pg = require("pg");
+const Pool = pg.Pool;
+
+const connectionString = process.env.DATABASE_URL || 'postgresql://localhost:5432/my_products_test';
+
+const pool = new Pool({
+    connectionString
+});
+
+describe('The balloon function', function(){
+
+
+    beforeEach(async function(){
+        // clean the tables before each test run
+        await pool.query("delete from;");
+    });
+    it('should get the valid color', function(){
+        assert.equal(, );
+    });
+    it('should get in-valid color', function(){
+        assert.equal(, );
+    });
+    after(function(){
+        pool.end();
+    })
+});
