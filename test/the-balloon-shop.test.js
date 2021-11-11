5e04134c58fa5407fcf0e44fc811627bd98db06b
Vhonani24 <vhonani.ramu@gmail.com>
2021-11-11 22:02:52 +0200

    first commit

--- test/the-balloon-shop.test.js
+++ test/the-balloon-shop.test.js
@@ -1,91 +1,104 @@
 let assert = require("assert");
-let TheBalloonShop = require("../the-balloon-shop");
+let TheFruitBasket = require("../the-fruit-basket");
 const pg = require("pg");
 const Pool = pg.Pool;
 
-const connectionString = process.env.DATABASE_URL || 'postgresql://vhonani:vhonani123@localhost:5432/my_balloon_tests';
+const connectionString = process.env.DATABASE_URL || 'postgresql://vhonani:vhonani123@localhost:5432/fruit_basket_tests';
 
 const pool = new Pool({
     connectionString
 });
 
-describe('The balloon function', function () {
+describe('The fruit_basket function', function () {
 
 
     beforeEach(async function () {
         // clean the tables before each test run
-        // await pool.query("delete from valid_colors;");
+        await pool.query("delete from fruit_basket");
         // add valid colors
     });
 
-    it('should get the valid colors', async function () {
+    it('should create a new fruit basket for a given fruit type, qty & fruit price', async function () {
 
-        const theBalloonShop = TheBalloonShop(pool, ['Orange', 'Purple', 'Lime']);
+        const testFruitBasket = TheFruitBasket(pool);
+
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+
+
+        assert.deepEqual([
+            {
+                "fruit_name": "Pears",
+                "price": "3.20",
+                "quantity": 5
+            }
+        ], await testFruitBasket.findAllFruitBaskets('Pears'));
+
+    });
+    it('should find all the fruit baskets for a given fruit type', async function () {
+
+        const testFruitBasket = TheFruitBasket(pool);
+
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+        await testFruitBasket.createFruitBasket('Apples', 4, 3.00);
+
+
+
+        assert.deepEqual([
+            {
+                "fruit_name": "Pears",
+                "price": "3.20",
+                "quantity": 5
+            }
+        ], await testFruitBasket.findAllFruitBaskets('Pears'));
+
+    });
+
+    it('should update the number of fruits in a given basket', async function () {
+
+        const testFruitBasket = TheFruitBasket(pool);
+
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+
+
+        var result = await testFruitBasket.updateFruitBasket('Pears', 5);
+
+        assert.deepEqual([
+            {
+                "fruit_name": "Pears",
+                "price": "3.20",
+                "quantity": 10
+            }
+        ], await testFruitBasket.findAllFruitBaskets('Pears'));
+    });
+    it('should show the total price for a given fruit basket', async function () {
+
+        const testFruitBasket = TheFruitBasket(pool);
+
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+
+
+        assert.deepEqual([{ sum: 16.00 } ], await testFruitBasket.showTotalFruitPrice('Pears'));
+    });
+    it('should show the sum of the total of the fruit baskets for a given fruit type', async function () {
+
+        const testFruitBasket = TheFruitBasket(pool);
+
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+        await testFruitBasket.createFruitBasket('Pears', 5, 3.20);
+        await testFruitBasket.createFruitBasket('Pears', 4, 3.20);
+
+        //console.log(await testFruitBasket.showSumOfTotalFruit_Baskets('Pears'));
+
+
+        assert.deepEqual([{ sum: 14 } ], await testFruitBasket.showSumOfTotalFruit_Baskets('Pears'));
+    });
     
-        assert.deepEqual(['Orange', 'Purple', 'Lime'], await theBalloonShop.getValidColors());
 
-    });
 
-    it('should get invalid colors', async function () {
-
-        const theBalloonShop = TheBalloonShop(pool, ['Orange', 'Purple', 'Lime']);
-
-        await theBalloonShop.requestColor('Blue');
-        await theBalloonShop.requestColor('Red');
-        await theBalloonShop.requestColor('Green');
-
-        assert.equal(['Blue', 'Red', 'Green'], theBalloonShop.getInvalidColors());
-
-    });
-
-    it('should return count for a specific color', async function () {
-        const theBalloonShop = TheBalloonShop(pool, ['Orange', 'Purple', 'Lime']);
-
-        await theBalloonShop.requestColor('Orange');
-        await theBalloonShop.requestColor('Orange');
-        await theBalloonShop.requestColor('Purple');
-        await theBalloonShop.requestColor('Orange');
-        await theBalloonShop.requestColor('Purple');
-        await theBalloonShop.requestColor('Orange');
-        await theBalloonShop.requestColor('Lime');
-
-        assert.equal(4, await theBalloonShop.colorCount('Orange'));
-        assert.equal(1, theBalloonShop.colorCount('Lime'));
-        assert.equal(2, await theBalloonShop.colorCount('Purple'));
-
-    })
-
-    it('should get all the colors - valid & invalid', async function () {
-
-        const theBalloonShop = TheBalloonShop(pool, ['Orange', 'Purple', 'Lime']);
-
-        await theBalloonShop.requestColor('Blue')
-        await theBalloonShop.requestColor('Red')
-
-        assert.equal(['Orange', 'Purple', 'Lime', 'Blue', 'Red'], await theBalloonShop.allColors());
-
-    })
-
-    it('an invalid color should become a valid color after 5 requests', async function () {
-
-        const theBalloonShop = TheBalloonShop(pool, []);
-
-        assert.equal([], await theBalloonShop.getValidColors());
-
-        await theBalloonShop.requestColor('Blue')
-        await theBalloonShop.requestColor('Blue')
-        await theBalloonShop.requestColor('Red')
-        await theBalloonShop.requestColor('Blue')
-        await theBalloonShop.requestColor('Blue')
-
-        assert.equal(['Blue', 'Red'], theBalloonShop.getInValidColors());
-
-        await theBalloonShop.requestColor('Blue')
-
-        assert.equal(['Blue'], await theBalloonShop.getValidColors());
-        assert.equal(['Red'], await theBalloonShop.getInValidColors());
-
-    });
+    
+   
+     
+   
 
     after(function () {
         pool.end();
---
+++ the-fruit-basket.js
@@ -0,0 +1,48 @@
+module.exports = (pool) => {
+
+
+    async function findAllFruitBaskets(fruit) {
+        const result = await pool.query('select fruit_name,price,quantity from fruit_basket where fruit_name = $1',[fruit]);
+
+        return result.rows;
+
+
+    }
+
+    async function showTotalFruitPrice(fruit){
+         var result =  await pool.query('SELECT SUM(price*quantity) FROM fruit_basket WHERE fruit_name = $1',[fruit]);
+         return result.rows;
+    }
+    
+    async function showSumOfTotalFruit_Baskets(fruit){
+        var result =  await pool.query('SELECT SUM(quantity) FROM fruit_basket WHERE fruit_name = $1',[fruit]);
+        return result.rows;
+   }
+
+
+
+    async function createFruitBasket(fruit, quantity, price) {
+
+        var result = await pool.query('select * from fruit_basket');
+
+        await pool.query("INSERT INTO fruit_basket(fruit_name,quantity,price) values($1,$2,$3)", [fruit, quantity, price])
+
+    }
+
+    async function updateFruitBasket (fruit,qty) {
+
+        var result =  await pool.query("UPDATE fruit_basket SET quantity = quantity + $2 where fruit_name = $1", [fruit,qty]);
+               
+    }
+
+    return {
+        createFruitBasket,
+        findAllFruitBaskets,
+        updateFruitBasket,
+       showTotalFruitPrice,
+       showSumOfTotalFruit_Baskets
+
+    }
+ 
+
+}
